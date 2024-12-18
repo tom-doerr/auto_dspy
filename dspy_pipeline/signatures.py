3b3167c3ed1cc18641474ea8592d855b16fdda23
Tom Dörr <tomdoerr96@gmail.com>
2024-12-18 12:35:03 +0100

    fix: Correct deepcopy and init for ChatCompletionSignature

--- dspy_pipeline/signatures.py
+++ dspy_pipeline/signatures.py
@@ -10,13 +10,11 @@ class ChatCompletionSignature(dspy.ChainOfThought):
     A signature for chat completion tasks.
     """
     def __init__(self, **kwargs):
-        super().__init__(**kwargs)
+        # super().__init__(**kwargs)
+        super().__init__(self, **kwargs)
 
     question = dspy.InputField()
     answer = dspy.OutputField()
-
-    def __deepcopy__(self, memo):
-        return self.__class__(**self.__dict__)
 
     def __deepcopy__(self, memo):
         return self.__class__(**self.__dict__)
